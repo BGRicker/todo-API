8c60248ffae156ce37176f0c028e31ee2f12dc8d
Ben Ricker <BGRicker@gmail.com>
2016-03-10 11:28:10 -0500

    description specs

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
   validates :title, presence: true
   validates :title, length: { in: 5..30 }
+  validates :description, presence: true
+  validates :description, length: { in: 5..30 }
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -66,4 +66,60 @@ describe 'creating a todo list' do
     visit "/todo_lists"
     expect(page).to_not have_content("Some cool stuff")
   end
+
+
+
+  it "displays an error when the there's no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    expect(page).to have_content "New Todo List"
+
+    fill_in "Title", with: "Some cool stuff"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content "error"
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Some cool stuff")
+  end
+
+  it "displays an error when the description isn't long enough" do
+    expect(TodoList.count).to eq(0)
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    expect(page).to have_content "New Todo List"
+
+    fill_in "Title", with: "Test Title"
+    fill_in "Description", with: "Test"
+    click_button "Create Todo list"
+
+    expect(page).to have_content "error"
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Some cool stuff")
+  end
+
+  it "displays an error when the title is too long" do
+    expect(TodoList.count).to eq(0)
+
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    expect(page).to have_content "New Todo List"
+
+    fill_in "Title", with: "Test Title"
+    fill_in "Description", with: "Some cool stuff"*50
+    click_button "Create Todo list"
+
+    expect(page).to have_content "error"
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Some cool stuff")
+  end
 end
