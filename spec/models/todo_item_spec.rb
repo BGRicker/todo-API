3c28402e614b6f8072ce0170e0cdd47d00f7858b
Ben Ricker <BGRicker@gmail.com>
2016-03-10 21:24:47 -0500

    shoulda errors, removal

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,5 +1,2 @@
 require 'spec_helper'
 
-describe TodoItem do
-  it { should belong_to(:todo_list) }
-end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,1 @@
 require 'spec_helper'
-
-describe TodoList do
-  it { should have_many(:todo_items) }
-end
