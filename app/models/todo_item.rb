f37ca2465bcfd87a5243cf9515b36aa20500b740
Ben Ricker <BGRicker@gmail.com>
2016-03-12 16:03:11 -0500

    completed todo items

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,4 +2,8 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   validates :content, presence: true
   validates :content, length: { in: 3..30 }
+
+  def completed?
+    !completed_at.blank?
+  end
 end
--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Editing todo items" do
-  let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
+  let!(:todo_list) { TodoList.create(title: "Your cool tasks", description: "buy pizza ingredients") }
   let!(:todo_item) { todo_list.todo_items.create(content: "Milk") }
 
   it "is successful when marking a single item complete" do
@@ -12,5 +12,21 @@ describe "Editing todo items" do
     end
     todo_item.reload
     expect(todo_item.completed_at).to_not be_nil
+  end
+
+  context "with completed items" do
+    let!(:completed_todo_item) { todo_list.todo_items.create(content: "made a pizza", completed_at: 5.minutes.ago) }
+    it "shows the completed items as complete" do
+      visit_todo_list todo_list
+      within dom_id_for(completed_todo_item) do
+        expect(page).to have_content(completed_todo_item.completed_at)
+      end
+    end
+    it "does not give the option to mark complete" do
+      visit_todo_list todo_list
+      within dom_id_for(completed_todo_item) do
+        expect(page).to_not have_content("Mark Complete")
+      end
+    end
   end
 end
