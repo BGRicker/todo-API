1497dc5941cd3dcf1e3f80f6dff715af298e399e
Ben Ricker <BGRicker@gmail.com>
2016-03-11 19:37:06 -0500

    todo items and spec

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,11 +2,33 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Your Cool List", description: "A good and cool list") }
-  it "displays no items when a todo list is empty" do
+
+  def visit_todo_list(list)
     visit "/todo_lists"
-    within "#todo_list_#{todo_list.id}" do
+    within "#todo_list_#{list.id}" do
       click_link "List Items"
     end
-    expect(page).to have_content("TodoItems#index")
+  end
+
+  it "displays the title of the todo list" do
+    visit_todo_list(todo_list)
+    within("h1") do
+      expect(page).to have_content(todo_list.title)
+    end
+
+  end
+
+  it "displays no items when a todo list is empty" do
+    visit_todo_list(todo_list)
+    expect(page.all("ul.todo_items li").size).to eq(0)
+  end
+
+  it "displays items when a todo list has items" do
+    todo_list.todo_items.create(content: "That's a spicy meatball")
+    visit_todo_list(todo_list)
+    expect(page.all("ul.todo_items li").size).to eq(1)
+    within "ul.todo_items" do
+      expect(page).to have_content("That's a spicy meatball")
+    end
   end
 end
