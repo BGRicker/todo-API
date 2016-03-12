6ddb20c2d4e98f6dd0e148728f03d295f50a812a
Ben Ricker <BGRicker@gmail.com>
2016-03-12 11:40:32 -0500

    todo items edit & spec

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -2,18 +2,6 @@ require 'spec_helper'
 describe "successfully creates todo items" do
   let!(:todo_list) { TodoList.create(title: "Your Cool List", description: "A good and cool list") }
 
-  def create_todo_item(options={})
-    options[:content] ||= "A good piece of content"
-
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-
-    fill_in "Content", with: "Pizza"
-    click_button "Save"
-  end
-
   def visit_todo_list(list)
     visit "/todo_lists"
     within "#todo_list_#{list.id}" do
---
+++ spec/features/todo_items/edit_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "editing todo items" do
+  let!(:todo_list) { TodoList.create(title: "Your Cool List", description: "A good and cool list") }
+  let!(:todo_item) { todo_list.todo_items.create(content: "get a pizza") }
+
+  def visit_todo_list(list)
+    visit "/todo_lists"
+    within "#todo_list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "edits a todo list with valid info" do
+    visit_todo_list(todo_list)
+    within("#todo_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: "dont forget pizza"
+    click_button "Save"
+    expect(page).to have_content("Added todo list item.")
+    todo_item.reload
+    expect(todo_item.content).to eq("dont forget pizza")
+  end
+
+  it "fails an edit with no content" do
+    visit_todo_list(todo_list)
+    within("#todo_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: ""
+    click_button "Save"
+    expect(page).to have_content("There was a problem adding that todo list item")
+    expect(page).not_to have_content("Saved todo list item")
+    todo_item.reload
+    expect(todo_item.content).to eq("get a pizza")
+  end
+
+  it "fails an edit with content thats too long" do
+    visit_todo_list(todo_list)
+    within("#todo_item_#{todo_item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: "this is long"*10
+    click_button "Save"
+    expect(page).to have_content("There was a problem adding that todo list item")
+    expect(page).not_to have_content("Saved todo list item")
+    todo_item.reload
+    expect(todo_item.content).to eq("get a pizza")
+  end
+
+end
