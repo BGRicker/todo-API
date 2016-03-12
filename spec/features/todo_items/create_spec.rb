7a20d87cd7fc17108c9a468a6914f4530df11e12
Ben Ricker <BGRicker@gmail.com>
2016-03-12 13:50:17 -0500

    extract todo list helper

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 describe "successfully creates todo items" do
   let!(:todo_list) { TodoList.create(title: "Your Cool List", description: "A good and cool list") }
 
-  def visit_todo_list(list)
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-  end
-
   it "creates a new todo" do
     visit_todo_list(todo_list)
     click_link "New Todo Item"
--- spec/features/todo_items/delete_spec.rb
+++ spec/features/todo_items/delete_spec.rb
@@ -4,13 +4,6 @@ describe "deletes todo items" do
   let!(:todo_list) { TodoList.create(title: "Your Cool List", description: "A good and cool list") }
   let!(:todo_item) { todo_list.todo_items.create(content: "get a pizza") }
 
-  def visit_todo_list(list)
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-  end
-
   it "is successful" do
     visit_todo_list(todo_list)
     within "#todo_item_#{todo_item.id}" do
