aee03ecc435defd4c044b0d94b0a6a1e500b4146
Ben Ricker <BGRicker@gmail.com>
2016-03-19 16:12:36 -0400

    user session controller/spec

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,13 @@ class UserSessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    user.authenticate(params[:password])
-    redirect_to todo_lists_path
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You did a good job logging in"
+      redirect_to todo_lists_path
+    else
+      flash[:error] = "This is a sad message for you"
+      render action: 'new'
+    end
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -34,6 +34,38 @@ describe UserSessionsController do
         expect(user).to receive(:authenticate)
         post :create, email: "rasheed@balldontlie.com", password: "refsarewrong1"
       end
+
+      it "sets the user_id in the session" do
+        post :create, email: "rasheed@balldontlie.com", password: "refsarewrong1"
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it "congratulates the user for logging in" do
+        post :create, email: "rasheed@balldontlie.com", password: "refsarewrong1"
+        expect(flash[:success]).to eq("You did a good job logging in")
+      end
+
+      it "is disappointed in a user who can't log in with no info" do
+        post :create
+        expect(flash[:error]).to eq("This is a sad message for you")
+      end
+
+      it "is disappointed in a user who can't log in without an email" do
+        post :create, password: "refsarewrong1"
+        expect(flash[:error]).to eq("This is a sad message for you")
+      end
+
+      it "is disappointed in a user who can't log in without a password" do
+        post :create, password: "refsarewrong1"
+        expect(flash[:error]).to eq("This is a sad message for you")
+      end
+
+      context "with blank credentials" do
+        it "renders the new template" do
+          post :create
+          expect(response).to render_template('new')
+        end
+      end
     end
   end
 end
